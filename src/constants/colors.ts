adc4bf81a518234a54dd40f344660231cece0dc9
Markos Mk <markosmk@me.com>
2023-09-03 01:48:01 -0300

    add: constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  green: '#129575',
+  white: '#fff',
+  black: '#000',
+  lightGrey: '#D9D9D9',
+  lightGrey2: '#A9A9A9',
+  grey: '#484848',
+};
+
+export default colors;
---
+++ src/constants/links.ts
@@ -0,0 +1,2 @@
+export const PRIVACY_LINK = 'https://google.com';
+export const TERMS_CONDITIONS_LINK = 'https://google.com';
